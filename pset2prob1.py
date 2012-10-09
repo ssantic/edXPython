703bd10c8ec814d4d73159534c88ae6fe0681531
Srdjan Santic <srdjan.santic@hotmail.com>
2012-10-09 14:10:29 +0200

    finished problem set 2 problem 1

--- pset2prob1.py
+++ pset2prob1.py
@@ -3,13 +3,27 @@ Problem set 2, Problem 1: Paying the Minimum
 
 A program that calculates the credit card balance after one year if a person
 pays the minimum monthly payment required by the credit card company each
-month.
+month. It prints out payments and remaining balances for each month, as well as
+the total paid amount, and the remaining balance at the end of the year.
 '''
-balance = 4213
-annualInterestRate = 0.2
-monthlyPaymentRate = 0.04
 
-monthlyInterestRate = monthlyPaymentRate/12 #Monthly interest rate
-minMonthlyPayment = monthlyPaymentRate * balance #Minimum monthly payment
-newBalance = (balance - minMonthlyPayment) * (1 + monthlyInterestrate)
+balance = 4213 #Starting debt balance
+annualInterestRate = 0.2 #Yearly interest rate in decimals
+monthlyPaymentRate = 0.04 #Monthly minimum payment rate in decimals
 
+
+monthlyInterestRate = annualInterestRate/12 #Monthly interest rate
+
+totalPaid = 0 #Sets the cumulation for total paid amount
+
+for month in range (1,13): #Reports payments for 12 months
+    minMonthlyPayment = monthlyPaymentRate * balance
+    balance = (balance - minMonthlyPayment) * (1 + monthlyInterestRate)
+    totalPaid = totalPaid + minMonthlyPayment
+    print ("Month: " + str(month))
+    print ("Minimum monthly payment: " + str(round(minMonthlyPayment,2)))
+    print ("Remaining balance: " + str(round(balance,2)))
+                           #End of for loop
+
+print ("Total paid: " + str(round(totalPaid,2)))
+print ("Remaining balance: " + str(round(balance,2)))
