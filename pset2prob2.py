a0c9587b5bb474003deeaaba0662264ca0978708
Srdjan Santic <srdjan.santic@hotmail.com>
2012-10-11 16:18:04 +0200

    finished problem set 2 problem 2

--- pset2prob2.py
+++ pset2prob2.py
@@ -3,25 +3,18 @@ This program calculates and prints out the minimum fixed monthly payment needed
 to pay off the total credit card balance within 12 months.
 '''
 
-balance = 3329
+balance = 4772
 annualInterestRate = 0.2
 
 monthlyInterestRate = annualInterestRate/12
 minMonthlyPayment = 10
+newBalance = balance
 
-while balance > 0:
- minMonthlyPayment = minMonthlyPayment + 10
+while newBalance > 0:
+ newBalance = balance
  for month in range (1,13):
-    balance = (balance - minMonthlyPayment) * (1 + monthlyInterestRate)
-
-
-
-
-
-
-
-
-
-
+    newBalance = (newBalance - minMonthlyPayment) * (1 + monthlyInterestRate)
+ if newBalance > 0:
+    minMonthlyPayment = minMonthlyPayment + 10
 
 print ("Lowest payment: " + str(minMonthlyPayment))
